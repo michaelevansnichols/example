2dc2b58293e540b2f6c2be7418c0befbbaecaccf
mnichols1 <mnichols@gitlab.com>
2019-04-17 11:11:48 -0400

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'GitLab ❤️ VuePress',
+    description: 'Vue-powered static site generator running on GitLab Pages makes Mike happy',
+    base: '/vuepress/',
+    dest: 'public',
+    themeConfig: {
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Page Two', link: '/page2'}
+        ]
+    }
+}
