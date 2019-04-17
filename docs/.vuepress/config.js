b254887095fedcd1e3a324815892bb6c86f1a737
mnichols1 <mnichols@gitlab.com>
2019-04-17 11:24:53 -0400

    Changed base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
 module.exports = {
     title: 'GitLab ❤️ VuePress',
     description: 'Vue-powered static site generator running on GitLab Pages makes Mike happy',
-    base: '/vuepress/',
     dest: 'public',
     themeConfig: {
         nav: [
